f95345bd2bcb3240f9f86485ce4efd4964289c73
Richard <die.heldeles@gmail.com>
2021-04-01 18:18:59 +0200

    Added correct .env

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from functools import wraps
 
 load_dotenv("../Environment_Variables/.env")
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager()
@@ -25,7 +25,7 @@ login_manager.init_app(app)
 
 ##CONNECT TO DB
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///blog.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
